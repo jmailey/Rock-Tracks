8aeecfe16b0b1c6bcafd7de47ede35b335f6340e
John Mailey <john.mailey@arnoldclark.com>
2019-02-11 19:18:19 +0000

    fix track page style

--- src/components/trackPage.js
+++ src/components/trackPage.js
@@ -3,28 +3,31 @@ export default class TrackPage extends Component {
   render() {
     const track = this.props.showingTrack;
     return (
-      <div>
-        <button onClick={() => this.props.back(false)}>Back</button>
-        <div className="track__artwork">
-          <img src={track.artworkUrl100} alt="" />
+      <div className="trackpage">
+        <button onClick={() => this.props.back(false)} className="back">
+          Go Back
+        </button>
+        <div class="track">
+          <div className="track__artwork">
+            <img src={track.artworkUrl100} alt="" />
+          </div>
+          <ul>
+            <li>{track.trackName}</li>
+            <li>{track.artistName}</li>
+            <li>£{track.collectionPrice}</li>
+            <li>
+              Duration: {Math.floor(track.trackTimeMillis / 60000)}:
+              {((track.trackTimeMillis % 60000) / 1000).toFixed(0)}
+            </li>
+            <li>released : {track.releaseDate}</li>
+            <li>
+              <a href={track.artistViewUrl}>Visit artist page</a>
+            </li>
+          </ul>
+          <div />
         </div>
-        <ul>
-          <li>{track.trackName}</li>
-          <li>{track.artistName}</li>
-          <li>£{track.collectionPrice}</li>
-          <li>
-            Duration: {Math.floor(track.trackTimeMillis / 60000)}:
-            {((track.trackTimeMillis % 60000) / 1000).toFixed(0)}
-          </li>
-          <li>released : {track.releaseDate}</li>
-          <li>
-            <a href={track.artistViewUrl}>Visit artist page</a>
-          </li>
-        </ul>
-        <div>
-          <p>Preview</p>
-          <audio src={track.previewUrl} controls />
-        </div>
+        <p>Preview</p>
+        <audio src={track.previewUrl} controls />
       </div>
     );
   }
